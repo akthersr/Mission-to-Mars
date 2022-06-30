484fc59ea6a81c7a01a81d8cea5ceba2d14e20e7
akthersr <103325522+akthersr@users.noreply.github.com>
2022-06-30 03:06:56 -0400

    Update Mission_to_Mars_Challenge.py

--- Mission_to_Mars_Challenge.py
+++ Mission_to_Mars_Challenge.py
@@ -1,62 +1,40 @@
-<<<<<<< HEAD:Mission_to_Mars_Challenge.py
 
-
-=======
->>>>>>> d537c6e284e11bb0a6dce6957085996cad34608d:Mission_to_Mars.py
 # Import Splinter and BeautifulSoup
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
-<<<<<<< HEAD:Mission_to_Mars_Challenge.py
+
 
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
 
-
-=======
 # Set up Splinter
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
 
->>>>>>> d537c6e284e11bb0a6dce6957085996cad34608d:Mission_to_Mars.py
 # Visit the mars nasa news site
 url = 'https://redplanetscience.com'
 browser.visit(url)
 # Optional delay for loading the page
 browser.is_element_present_by_css('div.list_text', wait_time=1)
 
-<<<<<<< HEAD:Mission_to_Mars_Challenge.py
-
-=======
->>>>>>> d537c6e284e11bb0a6dce6957085996cad34608d:Mission_to_Mars.py
 html = browser.html
 news_soup = soup(html, 'html.parser')
 slide_elem = news_soup.select_one('div.list_text')
 
-<<<<<<< HEAD:Mission_to_Mars_Challenge.py
-
-=======
->>>>>>> d537c6e284e11bb0a6dce6957085996cad34608d:Mission_to_Mars.py
 slide_elem.find('div', class_='content_title')
 
 # Use the parent element to find the first `a` tag and save it as `news_title`
 news_title = slide_elem.find('div', class_='content_title').get_text()
 news_title
 
-<<<<<<< HEAD:Mission_to_Mars_Challenge.py
-
-=======
->>>>>>> d537c6e284e11bb0a6dce6957085996cad34608d:Mission_to_Mars.py
 # Use the parent element to find the paragraph text
 news_p = slide_elem.find('div',class_ = 'article_teaser_body').get_text()
 news_p
 
 
 # ### Featured Images
-<<<<<<< HEAD:Mission_to_Mars_Challenge.py
 
-=======
->>>>>>> d537c6e284e11bb0a6dce6957085996cad34608d:Mission_to_Mars.py
 # Visit URL
 url = 'https://spaceimages-mars.com'
 browser.visit(url)
@@ -65,18 +43,10 @@ browser.visit(url)
 full_image_elem = browser.find_by_tag('button')[1]
 full_image_elem.click()
 
-<<<<<<< HEAD:Mission_to_Mars_Challenge.py
-
-=======
->>>>>>> d537c6e284e11bb0a6dce6957085996cad34608d:Mission_to_Mars.py
 # Parse the resulting html with soup
 html = browser.html
 img_soup = soup(html, 'html.parser')
 
-<<<<<<< HEAD:Mission_to_Mars_Challenge.py
-
-=======
->>>>>>> d537c6e284e11bb0a6dce6957085996cad34608d:Mission_to_Mars.py
 # Find the relative image url
 img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
 img_url_rel
@@ -85,10 +55,7 @@ img_url_rel
 img_url = f'https://spaceimages-mars.com/{img_url_rel}'
 img_url
 
-<<<<<<< HEAD:Mission_to_Mars_Challenge.py
 
-=======
->>>>>>> d537c6e284e11bb0a6dce6957085996cad34608d:Mission_to_Mars.py
 import pandas as pd
 
 df = pd.read_html('https://galaxyfacts-mars.com')[0]
@@ -96,15 +63,10 @@ df.columns=['description', 'Mars', 'Earth']
 df.set_index('description', inplace=True)
 df
 
-<<<<<<< HEAD:Mission_to_Mars_Challenge.py
+df.to_html()
 
 df.to_html()
 
-
-=======
-df.to_html()
-
->>>>>>> d537c6e284e11bb0a6dce6957085996cad34608d:Mission_to_Mars.py
 browser.quit()
 
 
